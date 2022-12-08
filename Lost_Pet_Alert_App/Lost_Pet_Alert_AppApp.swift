c91e0c86d31372e81ecd534207747a61f075a933
Erdem Inmez <inmezerdem@hotmail.com>
2022-12-08 16:52:46 -0500

    Initial Commit

---
+++ Lost_Pet_Alert_App/Lost_Pet_Alert_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lost_Pet_Alert_AppApp.swift
+//  Lost_Pet_Alert_App
+//
+//  Created by Erdem Inmez on 2022-12-08.
+//
+
+import SwiftUI
+
+@main
+struct Lost_Pet_Alert_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
